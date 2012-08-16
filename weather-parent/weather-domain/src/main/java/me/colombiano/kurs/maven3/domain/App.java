81cf533ec550bb3047a3c14d9ef90c143646e9b4
Andreas Pedro Garcia Vedal <me.colombiano@gmail.com>
2012-08-16 23:19:01 +0200

    created maven parent and modules projects

---
+++ weather-parent/weather-domain/src/main/java/me/colombiano/kurs/maven3/domain/App.java
@@ -0,0 +1,13 @@
+package me.colombiano.kurs.maven3.domain;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
